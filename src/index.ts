b9d50338140e80ed7a44f93cc4f01b64e963822d
shreyxnsh <shreyanshatwork@gmail.com>
2024-12-03 15:25:31 +0530

    login api completed

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 const app: Application = express();
 const PORT = process.env.PORT || 7000;
+import Router from "./routes/index.js";
 
 // * Middleware
 app.use(cors());
@@ -12,5 +13,8 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req: Request, res: Response) => {
   return res.send("It's working 🙌");
 });
+
+// Routes
+app.use("/api", Router);
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
